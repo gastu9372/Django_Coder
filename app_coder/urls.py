ea613a3c982c0fbe57979ff9f034e46768d586f2
Gaston Lorenzani <gastonlorenzani@gmail.com>
2024-12-15 16:11:47 -0300

    Sistema de logout y register + control de vistas

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,14 +1,20 @@
-from app_coder.views import *
+from app_coder import views
 from django.urls import path
+
 # Es buena practica escribir todas las funciones 1x1, buena practica lo explicito
 urlpatterns = [
-    path('inicio/', index, name="Inicio"),
-    path('vtubers/', vtubers, name ="Vtubers"),
-    path('mods/', mods, name="Mods"),
-    path('users/', users, name="Users"),
-    path('posts/', posts, name="Posts"),
-    path('formulario/', formulario_vtuber_api, name="Formulario"),
-    path('del_vtuber/<int:id>', eliminar_vtuber, name="Eliminar"),
-    path('edit_vtuber/<int:id>', editar_vtuber, name="Editar"),
-    path("login/", login_view, name = "Login" ),
+    path('inicio/', views.index, name="Inicio"),
+    path('vtubers/', views.vtubers, name ="Vtubers"),
+    path('mods/', views.mods, name="Mods"),
+    path('users/', views.users, name="Users"),
+    path('posts/', views.posts, name="Posts"),
+    path('formulario/', views.formulario_vtuber_api, name="Formulario"),
+    path('del_vtuber/<int:id>', views.eliminar_vtuber, name="Eliminar"),
+    path('edit_vtuber/<int:id>', views.editar_vtuber, name="Editar"),
+    
+    # Sesiones
+    path("login/", views.login_view, name = "Login" ),
+    path("logout/", views.user_logout, name = "Logout" ),
+    path("register/", views.register_view, name = "Register" ),
+    
 ]
--- app_coder/views.py
+++ app_coder/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.db.models import Q
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .forms import post_vtuber
 from .models import Vtuber
@@ -17,9 +21,24 @@ def login_view(request):
         if user is not None:
             login(request, user)
             return redirect("Inicio")
+        else:
+            return render(request, "app_coder/forms/login.html")
     else:
         return render(request, "app_coder/forms/login.html")
 
+def user_logout(request):
+    logout(request)
+    return redirect("Inicio")
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("Inicio")
+    else:
+        form = UserCreationForm()
+    return render(request, "app_coder/forms/register.html", {"form": form})
 
 def index(request):
     return render(request, "app_coder/index.html")
@@ -34,15 +53,19 @@ def vtubers(request):
         
     return render(request, "app_coder/vtubers.html", {"vtubers":vtubers})
 
+@login_required
 def mods(request):
     return render(request, "app_coder/mods.html")
 
+@login_required
 def users(request):
     return render(request, "app_coder/users.html")
 
+@login_required
 def posts(request):
     return render(request, "app_coder/posts.html")
 
+@login_required
 def formulario_vtuber_api(request):
     
     if request.method == "POST":
@@ -53,14 +76,15 @@ def formulario_vtuber_api(request):
     else:
         post_vtuber_form = post_vtuber()
         
-    
     contexto= {"post_vtuber": post_vtuber_form}
     return render(request, "app_coder/forms/formulario.html", contexto)
+
 
 def eliminar_vtuber(request, id):
     vtuber = Vtuber.objects.get(id=id)
     vtuber.delete()
     return redirect("Vtubers")
+
 
 def editar_vtuber(request, id):
     vtuber = Vtuber.objects.get(id=id)
